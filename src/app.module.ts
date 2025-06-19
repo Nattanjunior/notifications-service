a9b21e5e0f691cafdac190d960c89abccd0993e5
nattanjunior <fnatanieljunior@gmail.com>
2025-06-19 03:47:41 -0300

    refactor(app.module): ajusta configurações e importações do módulo principal da aplicação. Explica as mudanças estruturais realizadas.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, NotificationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
