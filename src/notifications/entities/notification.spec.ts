09b6aef86c6b06ab9ed8b813fe548e5ca2a7e0e3
nattanjunior <fnatanieljunior@gmail.com>
2025-06-19 20:00:29 -0300

    test(domain): add unit tests for entities
    Adds unit tests for Notification and Content entities, ensuring their behavior and validation rules are correct.

---
+++ src/notifications/entities/notification.spec.ts
@@ -0,0 +1,15 @@
+import { Content } from './content';
+import { Notification } from './notifications';
+import { describe, expect, it } from '@jest/globals';
+
+describe('Notification', () => {
+  it('should be able to create a notification', () => {
+    const notification = new Notification({
+      content: new Content('Nova solicitação de amizade'),
+      category: 'social',
+      recipientId: 'example-recipient-id',
+    });
+
+    expect(notification).toBeTruthy();
+  });
+});
