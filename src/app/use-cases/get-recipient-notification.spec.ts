1a8afc4b242993b5d106c0b56777b9b66af7fb70
nattanjunior <fnatanieljunior@gmail.com>
2025-06-21 20:37:49 -0300

    refactor(test): use notification factory in specs

--- src/app/use-cases/get-recipient-notification.spec.ts
+++ src/app/use-cases/get-recipient-notification.spec.ts
@@ -5,7 +5,7 @@ import { describe, expect, it } from '@jest/globals';
 import { makeNotification } from '@test/factories/notifications-factory';
 import { GetRecipientNotification } from './get-recipient-notifications';
 
-describe('Count recipients notifications', () => {
+describe('Get notifications', () => {
   it('should be able to get recipient notifications', async () => {
     const notificationsRepository = new InMemoryNotificationsRepository();
     const getRecipientNotifications = new GetRecipientNotification(
--- src/app/use-cases/send-notification.spec.ts
+++ src/app/use-cases/send-notification.spec.ts
@@ -1,9 +1,10 @@
 import { describe, expect, it } from '@jest/globals';
 import { SendNotification } from './Send-notifications';
-import { NotificationRepository } from '@app/repositories/notifications-repository';
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
 
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
+    const NotificationRepository = new InMemoryNotificationsRepository()
     const sendNotification = new SendNotification(NotificationRepository);
 
     const { notification } = await sendNotification.execute({
