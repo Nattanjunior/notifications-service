cbf8b1234bb48780350fb85e5df64a3d7c8a1df7
nattanjunior <fnatanieljunior@gmail.com>
2025-06-21 19:17:08 -0300

    test(use-case): add tests for count recipient notifications

---
+++ src/app/use-cases/count-recipient-notifications.spec.ts
@@ -0,0 +1,35 @@
+import { Content } from '@app/entities/content';
+import { Notification } from '@app/entities/notifications';
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { CountRecipientNotification } from './count-recipient-notification';
+import { describe, expect, it } from '@jest/globals';
+import { makeNotification } from '@test/factories/notifications-factory';
+
+describe('Count recipients notifications', () => {
+  it('should be able to count recipient a notifications', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const countRecipientNotifications = new CountRecipientNotification(
+      notificationsRepository,
+    );
+
+
+
+    await notificationsRepository.create(
+      makeNotification({recipientId: 'recipient-1'})
+    );
+
+    await notificationsRepository.create(
+      makeNotification({recipientId: 'recipient-1'})
+    );
+
+    await notificationsRepository.create(
+      makeNotification({recipientId: 'recipient-2'})
+    );
+
+    const { count } = await countRecipientNotifications.execute({
+      recipientId: 'recipientId-1'
+    });
+
+    expect(count).toEqual(2);
+  });
+});
