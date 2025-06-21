8970c0bc76c6f2e7f186969a29467b1dd4928009
nattanjunior <fnatanieljunior@gmail.com>
2025-06-21 20:37:29 -0300

    feat(http): implement all notification endpoints and use cases

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,11 +1,66 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { CreateNotificationDto } from '../../../infra/http/dto/create-notification.dto';
-import { SendNotification } from '../../../app/use-cases/Send-notifications';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { SendNotification } from '@app/use-cases/Send-notifications';
+import { CreateNotificationDto } from '../dto/create-notification.dto';
 import { NotificationViewModel } from '../view-models/notification-view-models';
+import { CancelNotification } from '@app/use-cases/cancel-notification';
+import { ReadNotification } from '@app/use-cases/read-notification';
+import { UnreadNotification } from '@app/use-cases/unread-notification';
+import { CountRecipientNotification } from '@app/use-cases/count-recipient-notification';
+import { GetRecipientNotification } from '@app/use-cases/get-recipient-notifications';
 
 @Controller('notifications')
 export class NotificationsController {
-  constructor(private sendNotification: SendNotification) { }
+  constructor(
+    private sendNotification: SendNotification,
+    private cancelNotification: CancelNotification,
+    private readNotification: ReadNotification,
+    private unreadNotification: UnreadNotification,
+    private countRecipientNotification: CountRecipientNotification,
+    private getRecipientNotification: GetRecipientNotification,
+  ) { }
+
+  @Patch(':id/cancel')
+  async cancel(@Param('id') id: string) {
+    await this.cancelNotification.execute({
+      notificationId: id
+    })
+  }
+
+  @Get('count/from/:recipientId')
+  async countFromRecipient(@Param('recipientId') recipientId: string) {
+    const { count } = await this.countRecipientNotification.execute({
+      recipientId,
+    });
+
+    return {
+      count,
+    };
+  }
+
+  @Get('from/:recipientId')
+  async getFromRecipient(@Param('recipientId') recipientId: string) {
+    const { notification } = await this.getRecipientNotification.execute({
+      recipientId,
+    });
+
+    return {
+      notifications: notification.map(NotificationViewModel.toHTTP),
+    };
+  }
+
+  @Patch(':id/read')
+  async read(@Param('id') id: string) {
+    await this.readNotification.execute({
+      notificationId: id
+    })
+  }
+
+  @Patch(':id/unread')
+  async unread(@Param('id') id: string) {
+    await this.unreadNotification.execute({
+      notificationId: id
+    })
+  }
 
   @Post()
   async create(@Body() body: CreateNotificationDto) {
@@ -18,7 +73,7 @@ export class NotificationsController {
     });
 
     return {
-      notification: NotificationViewModel.toHTTP(notification)
+      notification: NotificationViewModel.toHTTP(notification),
     };
   }
 }
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,10 +3,22 @@ import { Module } from '@nestjs/common';
 import { NotificationsController } from './controllers/notifications.controller';
 import { SendNotification } from '@app/use-cases/Send-notifications';
 import { DatabaseModule } from '../database/database.module';
+import { CancelNotification } from '@app/use-cases/cancel-notification';
+import { ReadNotification } from '@app/use-cases/read-notification';
+import { CountRecipientNotification } from '@app/use-cases/count-recipient-notification';
+import { UnreadNotification } from '@app/use-cases/unread-notification';
+import { GetRecipientNotification } from '@app/use-cases/get-recipient-notifications';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    ReadNotification,
+    UnreadNotification,
+    CountRecipientNotification,
+    GetRecipientNotification,
+  ],
 })
-export class HttpModule {}
+export class HttpModule { }
