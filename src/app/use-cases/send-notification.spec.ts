344c68aa729c3e3fcf2defc52a30bb898babd1ba
nattanjunior <fnatanieljunior@gmail.com>
2025-06-21 16:48:10 -0300

    fix(test): correct send-notification test setup

--- src/app/use-cases/send-notification.spec.ts
+++ src/app/use-cases/send-notification.spec.ts
@@ -1,9 +1,10 @@
 import { describe, expect, it } from '@jest/globals';
 import { SendNotification } from './Send-notifications';
+import { NotificationRepository } from '@app/repositories/notifications-repository';
 
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(NotificationRepository);
 
     const { notification } = await sendNotification.execute({
       content: 'This is a notification',
