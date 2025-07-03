248126d57f5637dfacfcae54e769b2228a71d2cb
nattanjunior <fnatanieljunior@gmail.com>
2025-07-03 16:45:58 -0300

    feat(dto): adiciona decorators do Swagger ao CreateNotificationDto

--- src/infra/http/dto/create-notification.dto.ts
+++ src/infra/http/dto/create-notification.dto.ts
@@ -1,16 +1,20 @@
 import { IsNotEmpty, IsUUID, Length } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 
 export class CreateNotificationDto {
   @IsNotEmpty()
   @IsUUID()
+  @ApiProperty()
   recipientId: string;
 
   @IsNotEmpty()
   @Length(5, 240)
+  @ApiProperty()
   content: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   category: string;
 }
 
