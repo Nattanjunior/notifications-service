fc7b5191b96016ace937428c90a2ee0e5e09788a
nattanjunior <fnatanieljunior@gmail.com>
2025-07-03 16:46:06 -0300

    feat(core): integra Swagger para documentação automática da API em /docs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  
+  const config = new DocumentBuilder()
+    .setTitle('Notifications API')
+    .setDescription('API REST para envio de notificações')
+    .setVersion('1.0')
+    .addTag('notifications')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
 
   app.useGlobalPipes(new ValidationPipe());
 
