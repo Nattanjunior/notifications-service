915c8f23ec46ca6c8913cf879243ee514c548c9f
nattanjunior <fnatanieljunior@gmail.com>
2025-09-24 01:21:05 -0300

    refactor(cancel-notification): remove unused imports from cancel notification use case

--- src/app/use-cases/cancel-notification.ts
+++ src/app/use-cases/cancel-notification.ts
@@ -1,6 +1,4 @@
 import { NotificationRepository } from 'src/app/repositories/notifications-repository';
-import { Content } from '../entities/content';
-import { Notification } from '../entities/notifications';
 import { Injectable } from '@nestjs/common';
 import { NotificationFound } from './errors/notification-not-found';
 
